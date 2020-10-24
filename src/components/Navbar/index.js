1f6bec630679c90cbd94255ede0c60f4c2c71118
Vladyslav <vladost000@gmail.com>
2020-10-24 16:44:35 +0300

    add sidebar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -12,13 +12,13 @@ import {
 } from './NavbarElements';
 
 import { FaBars } from 'react-icons/fa';
-const Navbar = () => {
+const Navbar = ({toggle}) => {
     return (
        <>
          <Nav>
             <NavbarContainer>
                 <NavLogo to="/">dolla</NavLogo>
-                <MobileIcon>
+                <MobileIcon onClick={toggle}>
                     <FaBars />
                 </MobileIcon>
                 <NavMenu>
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,12 +1,33 @@
-import React from 'react'
+import React from 'react';
+import {
+    SidebarContainer,
+    Icon,
+    CloseIcon,
+    SidebarRoute,
+    SideBtnWrap,
+    SidebarLink,
+    SidebarWrapper,
+    SidebarMenu
+} from './SidebarElements';
 
-const Sidebar = () => {
+const Sidebar = ({isOpen, toggle}) => {
     return (
-       <SidebarConatainer>
-          <Icon>
+       <SidebarContainer isOpen={isOpen} onClick={toggle}>
+          <Icon onClick={toggle}>
              <CloseIcon />    
           </Icon> 
-       </SidebarConatainer>
+          <SidebarWrapper>
+            <SidebarMenu>
+                <SidebarLink onClick={toggle} to="about">About</SidebarLink>
+                <SidebarLink onClick={toggle} to="discover">Discover</SidebarLink>
+                <SidebarLink onClick={toggle} to="services">Services</SidebarLink>
+                <SidebarLink onClick={toggle} to="signup">Sign up</SidebarLink>
+            </SidebarMenu>
+            <SideBtnWrap>
+                <SidebarRoute to="/signin" >Sign in</SidebarRoute>   
+            </SideBtnWrap>   
+          </SidebarWrapper>
+       </SidebarContainer>
     )
 }
 
